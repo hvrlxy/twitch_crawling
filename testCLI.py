56dfc7136ae21acb9e76654a72bddad44e77b3d3
hvrlxy <hle5@gustavus.edu>
2021-06-06 17:44:13 +0700

    second commit

--- testCLI.py
+++ testCLI.py
@@ -1,7 +1,7 @@
 # this program will run the crawling process for 2 hours, kill the process and run it again
 import os, signal
 
-for i in range (12):
-    process = os.system('gtimeout 7200 python3 user_data.py > stream_data.txt')
+for i in range (48):
+    process = os.system('gtimeout 1800 python3 user_data.py')
 #    print('end one round')
 # os.kill(process, signal.SIGKILL)
--- user_data.py
+++ user_data.py
@@ -1,6 +1,5 @@
-# this program go through the list of 10000 user ids, grasp every stream currently occur
-# and write it down in the stream_data.txt
-import twitch
+import twitch, datetime
+from datetime import timezone
 
 client_id = 'gp762nuuoqcoxypju8c569th9wz7q5'
 oath_token = 'nv8v0sdhbaebvqj6dcez1iy14nvogw'
@@ -10,31 +9,33 @@ client = twitch.TwitchHelix(client_id=client_id, oauth_token=oath_token)
 user_file = open('data.txt', 'r')
 user_data = user_file.readlines()
 user_file.close()
-num = len(user_data)
+num = 2600
 
 stream_file = open('stream_data.txt','a+')
+inactive_file = open('inactive.txt', 'a+')
 
 for i in range(num):
 	user_data[i] = user_data[i].replace('\n','')
 
-i = 0
-while(True):
-	# since the api only let us get stream info from 20 users at a time, we increment 
-	# i by 20 at a time
-	streams = client.get_streams(user_ids=user_data[i:i+20])
-	for stream in streams:
-		user = stream['user_id']
-		stream_start = stream['started_at']
-		stream_game = stream['game_name']
-		stream_gameid = stream['game_id']
-		stream_lang = stream['language']
-		stream_viewer = stream['viewer_count']
-		user_followers = client.get_user_follows(to_id=user)
-		user_follow = user_followers.total
+for i in range (num):
+	try:
+		stream = client.get_streams(user_ids=user_data[i])
+		user_follow = client.get_user_follows(to_id=user_data[i]).total
+		current_time = datetime.datetime.utcnow()
 
-		stream_file.write(user + ',' + str(stream_start) + ',' + stream_game + ',' + stream_gameid 
-			+ ',' + stream_lang + ',' + str(stream_viewer) + ',' + str(user_follow) + '\n')
-	i = i + 20
-	if i > 9999:
-		i = 0
+		if len(stream) > 0:
+			stream_id = stream[0]['id']
+			stream_start = stream[0]['started_at']
+			stream_game = stream[0]['game_name']
+			stream_gameid = stream[0]['game_id']
+			stream_lang = stream[0]['language']
+			stream_viewer = stream[0]['viewer_count']
+			dt = datetime.datetime.now()
+
+			print(stream_id + ',' + user_data[i] + ',' + str(stream_start) + ',' + stream_game + ',' + stream_gameid 
+				+ ',' + stream_lang + ',' + str(stream_viewer) + ',' + str(user_follow) + ',' + str(current_time) + '\n')
+		else:
+			inactive_file.write(user_data[i] + ',' + str(user_follow) + ',' + str(current_time) + '\n')
+	except twitch.exceptions.TwitchNotProvidedException:
+		continue
 
