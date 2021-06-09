e2030bdcedad36d3c62c1c5a56df7d53f155a01d
hvrlxy <hle5@gustavus.edu>
2021-06-09 20:24:24 +0700

    third commit

--- testCLI.py
+++ testCLI.py
@@ -1,7 +1,20 @@
 # this program will run the crawling process for 2 hours, kill the process and run it again
-import os, signal
+import subprocess
 
-for i in range (48):
-    process = os.system('gtimeout 1800 python3 user_data.py')
-#    print('end one round')
-# os.kill(process, signal.SIGKILL)
+try:
+    while(True):
+        print()
+        print("Process Begain")
+        subprocess.run('gtimeout 1800 python3 user_data.py', shell=True)
+        subprocess.run('gtimeout 1800 python3 user_data.py', shell=True)
+        subprocess.run('gtimeout 1800 python3 user_data.py', shell=True)
+        subprocess.run('gtimeout 1800 python3 user_data.py', shell=True)
+        subprocess.run('gtimeout 1800 python3 user_data2.py', shell=True)
+        subprocess.run('gtimeout 1800 python3 user_data2.py', shell=True)
+        subprocess.run('gtimeout 1800 python3 user_data2.py', shell=True)
+        subprocess.run('gtimeout 1800 python3 user_data2.py', shell=True)
+        
+
+except KeyboardInterrupt:
+       print("Keyboard Interrupt!")
+
